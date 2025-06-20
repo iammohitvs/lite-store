8b77cbc8318275a63505268c29e83881adfa55d1
Mohit V S <vsmohit94@gmail.com>
2025-06-20 11:20:31 +0530

    made a created_entry function for the hash table

--- in-memory-store/hash_table.c
+++ in-memory-store/hash_table.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "utils.h"
 
@@ -9,11 +10,75 @@ HashTable hash_table = {
     .capacity = MAX_HASH_TABLE_CAPACITY
 };
 
+int create_entry(char *name, char* value){
+    if(hash_table.size == hash_table.capacity){
+        printf("hash table is full");
+        return -2;
+    }
 
+    if(!name || !value){
+        perror("name or value not found (create entry)");
+        return -1;
+    }
 
-int main(int argc, char const *argv[])
+    // get the index
+    int index = get_hash_table_index(name);
+    printf("INDEX FOR THE NAME: %d\n", index);
+
+    HashTableEntry *entry = &hash_table.hashTableValues[index];
+    HashTableEntry *prev = NULL;
+
+    // update the value if a name exists already
+    while (entry && entry->name != NULL)
+    {
+        if(strcmp(entry->name, name) == 0){
+            entry->value = value;
+            return 0;
+        }
+
+        prev = entry;
+        entry = entry->next;
+    }
+    
+
+    HashTableEntry *new_entry = malloc(sizeof(HashTableEntry));;
+    if (!new_entry){
+        perror("Failed to malloc for new entry (create entry)");
+        return -1;
+    }
+
+    new_entry->name = name;
+    new_entry->value = value;
+    new_entry->next = NULL;
+    
+    if(prev){
+        // add to the end of the linked list
+        prev->next = new_entry;
+    } else {
+        // add a fully new entry
+        hash_table.hashTableValues[index] = *new_entry;
+    }
+    
+    hash_table.size += 1;
+    return 0;
+}
+
+// testing the create_entry function
+/* int main(int argc, char const *argv[])
 {
     printf("\nINITIALISED HASH TABLE\n:");
     printf("HASH TABLE CAPACITY: %d\n", hash_table.capacity);
+
+    char *name = "Manager";
+    char *value = "Micheal Scott";
+
+    create_entry(name , value);
+    int index = get_hash_table_index(name);
+
+    printf("CHECKING IN MAIN: \n");
+    printf("TABLE INDEX: %d\n", index);
+    printf("TABLE NAME FOR INDEX: %s\n", hash_table.hashTableValues[index].name);
+    printf("TABLE VALUE FOR INDEX: %s\n", hash_table.hashTableValues[index].value);
+
     return 0;
-}
+} */
